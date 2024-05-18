3d047953483fcbc8b8f1d97ef6f8350835c9e5fb
eguturisai2610 <eguturisai26@gmail.com>
2024-05-18 15:02:12 +0530

    first commit

---
+++ src/main/java/com/sai/seviceRegistry/SeviceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.sai.seviceRegistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeviceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeviceRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sai/seviceRegistry/SeviceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sai.seviceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeviceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
